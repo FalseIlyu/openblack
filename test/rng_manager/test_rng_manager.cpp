b7507548976b0bcf648ffcc62057dedb5ddf3ecc
FalseIlyu <ilyulemail@gmail.com>
2022-04-23 16:57:53 +0200

    Test for predictable result in debug mode

--- test/rng_manager/test_rng_manager.cpp
+++ test/rng_manager/test_rng_manager.cpp
@@ -10,13 +10,53 @@
 #include <gtest/gtest.h>
 
 #include <Resources/RNGManager.h>
+#include <vector>
+
+#define N_TEST 1000000
+#define TEST_SEED 15000
+
 
 TEST(RngManager, debug_predictable_mono)
 {
+    openblack::resources::RNGManager& rngmanager = openblack::resources::RNGManager::GetInstance();
+    ASSERT_TRUE(rngmanager.SetDebugMode(true, TEST_SEED));
 
+    std::vector<uint16_t> fstResult(N_TEST);
+    for (auto it=fstResult.begin(); it!=fstResult.end(); ++it)
+    {
+        *it = rngmanager.nextInt();
+    }
+
+    // Two run with the same seed mush give the same vector.
+    ASSERT_TRUE(rngmanager.SetDebugMode(true, TEST_SEED));
+    std::vector<uint16_t> sndResult(N_TEST);
+    for (auto it=sndResult.begin(); it!=sndResult.end(); ++it)
+    {
+        *it = rngmanager.nextInt();
+    }
+    ASSERT_TRUE(fstResult==sndResult);
 }
 
 TEST(RngManager, debug_predictable_multi)
 {
+    openblack::resources::RNGManager& rngmanager = openblack::resources::RNGManager::GetInstance();
+    ASSERT_TRUE(rngmanager.SetDebugMode(true, TEST_SEED));
 
+    std::vector<uint16_t> fstResult(N_TEST);
+    for (auto it=fstResult.begin(); it!=fstResult.end(); ++it)
+    {
+        *it = rngmanager.nextInt();
+    }
+
+    /*
+     * TODO must guarentee that with multiple thread using RNGManager in debug
+     * mode will use the same sequence of number.
+     */
+    ASSERT_TRUE(rngmanager.SetDebugMode(true, TEST_SEED));
+    std::vector<uint16_t> sndResult(N_TEST);
+    for (auto it=sndResult.begin(); it!=sndResult.end(); ++it)
+    {
+        *it = rngmanager.nextInt();
+    }
+    ASSERT_TRUE(fstResult==sndResult);
 }
