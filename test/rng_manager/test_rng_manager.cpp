8561e52d6d29b360990ce7f25f148c06360fcfed
FalseIlyu <ilyulemail@gmail.com>
2022-04-23 17:41:21 +0200

    Multithreaded test for debug RNGManager

--- test/rng_manager/test_rng_manager.cpp
+++ test/rng_manager/test_rng_manager.cpp
@@ -10,10 +10,14 @@
 #include <gtest/gtest.h>
 
 #include <Resources/RNGManager.h>
+#include <iostream>
+#include <mutex>
+#include <thread>
 #include <vector>
 
-#define N_TEST 1000000
+#define N_TEST 100
 #define TEST_SEED 15000
+#define N_THREAD 10
 
 
 TEST(RngManager, debug_predictable_mono)
@@ -37,6 +41,23 @@ TEST(RngManager, debug_predictable_mono)
     ASSERT_TRUE(fstResult==sndResult);
 }
 
+void randomThread(int totalIteration, int& counter, std::vector<uint16_t>& result, std::mutex& lock) 
+{
+    openblack::resources::RNGManager& rngmanager = openblack::resources::RNGManager::GetInstance();
+    
+    for (int i = 0; i < totalIteration; i++)
+    {
+        uint16_t random = rngmanager.nextInt();
+        {
+            std::lock_guard<std::mutex> locked(lock);
+            std::cout << counter << ": ";
+            std::cout << random << std::endl;
+            result[counter] = random;
+            counter++;
+        }
+    }
+};
+
 TEST(RngManager, debug_predictable_multi)
 {
     openblack::resources::RNGManager& rngmanager = openblack::resources::RNGManager::GetInstance();
@@ -52,11 +73,22 @@ TEST(RngManager, debug_predictable_multi)
      * TODO must guarentee that with multiple thread using RNGManager in debug
      * mode will use the same sequence of number.
      */
+    
     ASSERT_TRUE(rngmanager.SetDebugMode(true, TEST_SEED));
+    int index = 0;
+    int item_per_thread = N_TEST / N_THREAD;
     std::vector<uint16_t> sndResult(N_TEST);
-    for (auto it=sndResult.begin(); it!=sndResult.end(); ++it)
+    std::vector<std::thread> thread_vector;
+    std::mutex lock;
+    for (int i = 0; i < N_THREAD; i++)
     {
-        *it = rngmanager.nextInt();
+        auto l = [item_per_thread, &index, &sndResult, &lock] { randomThread(item_per_thread, index, sndResult, lock); };
+        thread_vector.push_back(std::thread(l));
     }
+    for (auto t = thread_vector.begin(); t != thread_vector.end(); ++t)
+    {
+        t ->join();
+    }
+
     ASSERT_TRUE(fstResult==sndResult);
 }
